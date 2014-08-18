9321cdbb74ee01b07482642af7c5b6e254e307be
Sophie0608 <limei0608@yahoo.fr>
2014-08-18 16:01:46 +0800

    18th

---
+++ Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,161 @@
+﻿for(var i = 0; i < 75; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+u50.tabIndex = 0;
+
+u50.style.cursor = 'pointer';
+$axure.eventManager.click('u50', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u51','','none',500);
+
+	SetPanelState('u51', 'pd0u51','none','',500,'none','',500);
+
+}
+});
+u52.tabIndex = 0;
+
+u52.style.cursor = 'pointer';
+$axure.eventManager.click('u52', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u51','hidden','none',500);
+
+	SetPanelVisibility('u40','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u54'] = 'center';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u26'] = 'center';u27.tabIndex = 0;
+
+u27.style.cursor = 'pointer';
+$axure.eventManager.click('u27', function(e) {
+
+if (true) {
+
+	SetPanelState('u4', 'pd1u4','none','',500,'none','',500);
+
+	SetPanelVisibility('u4','','none',500);
+
+}
+});
+u28.tabIndex = 0;
+
+u28.style.cursor = 'pointer';
+$axure.eventManager.click('u28', function(e) {
+
+if (true) {
+
+	SetPanelState('u4', 'pd2u4','none','',500,'none','',500);
+
+	SetPanelVisibility('u4','','none',500);
+
+}
+});
+u29.tabIndex = 0;
+
+u29.style.cursor = 'pointer';
+$axure.eventManager.click('u29', function(e) {
+
+if (true) {
+
+	SetPanelState('u4', 'pd3u4','none','',500,'none','',500);
+
+	SetPanelVisibility('u4','','none',500);
+
+}
+});
+gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u64'] = 'top';u65.tabIndex = 0;
+
+u65.style.cursor = 'pointer';
+$axure.eventManager.click('u65', function(e) {
+
+if (true) {
+
+	SetPanelState('u51', 'pd1u51','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u68'] = 'center';u30.tabIndex = 0;
+
+u30.style.cursor = 'pointer';
+$axure.eventManager.click('u30', function(e) {
+
+if (true) {
+
+	SetPanelState('u4', 'pd0u4','none','',500,'none','',500);
+
+	SetPanelVisibility('u4','','none',500);
+
+}
+});
+gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u35'] = 'center';u36.tabIndex = 0;
+
+u36.style.cursor = 'pointer';
+$axure.eventManager.click('u36', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u31','hidden','none',500);
+
+	SetPanelVisibility('u66','hidden','none',500);
+
+	SetPanelVisibility('u4','','none',500);
+
+	SetPanelState('u4', 'pd2u4','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u38'] = 'center';u39.tabIndex = 0;
+
+u39.style.cursor = 'pointer';
+$axure.eventManager.click('u39', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u66','','none',500);
+
+}
+});
+gv_vAlignTable['u70'] = 'center';gv_vAlignTable['u72'] = 'center';gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u74'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u6'] = 'center';document.getElementById('u8_img').tabIndex = 0;
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u40','','none',500);
+
+	SetPanelVisibility('u66','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'center';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u46'] = 'center';u47.tabIndex = 0;
+
+u47.style.cursor = 'pointer';
+$axure.eventManager.click('u47', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u51','hidden','none',500);
+
+	SetPanelVisibility('u40','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';u14.tabIndex = 0;
+
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u31','','none',500);
+
+}
+});
+gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u18'] = 'center';
